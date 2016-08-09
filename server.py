675603277267241127770b872292527105d0b6cf
Laurel <lkorwin@gmail.com>
2016-08-09 00:44:06 +0000

    Modified search function

--- server.py
+++ server.py
@@ -6,12 +6,13 @@ from model import connect_to_db, db, User, Book, Rating, Board
 import os
 import lxml
 from amazonproduct import API
+import collections
 
 access_key = os.environ['AWS_ACCESS_KEY_ID']
 secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
 associate_tag = os.environ['AWS_ASSOCIATE_TAG']
 
-api = API(locale='us', access_key_id=access_key, secret_access_key=secret_key, associate_tag=associate_tag)
+api = API(locale='us', access_key_id=access_key, secret_access_key=secret_key, associate_tag=associate_tag) #sets up api gateway w/keys above
 
 app = Flask(__name__)
 
@@ -46,10 +47,10 @@ def process_login():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    user_exists = User.query.filter_by(user_name=username).first()
+    user_exists = User.query.filter_by(user_name=username).first()  #gets first result if username exists in DB, otherwise gets None.
 
-    if user_exists != None and user_exists.password == password:
-        flash('Successfully logged in!')
+    if user_exists != None and user_exists.password == password:    #checks to see if user is in DB, and is password matches.
+        flash('Successfully logged in!')                            #If not, redirects to login / register accordingly.
         session['logged_in'] = user_exists.user_id
         return redirect('/')
     elif user_exists != None and user_exists.password != password:
@@ -59,11 +60,13 @@ def process_login():
         flash('User account not found. Please register.')
         return redirect('/register')
 
+
 @app.route('/register')
 def register_user():
     """Process registration information"""
 
     return render_template('register.html')
+
 
 @app.route('/process_registration', methods=['POST'])
 def process_registration():
@@ -74,32 +77,33 @@ def process_registration():
 
     user_exists = User.query.filter_by(user_name=username).first()
 
-    if user_exists != None:
+    if user_exists != None:                                     #Validates to see if username is in database (in case someone tries to pick one already in use)
         flash('Oops, that user name is already registered. Please choose another.')
         return redirect('/register')
     else:
-        new_user = User(user_name=username, email=email, password=password)
+        new_user = User(user_name=username, email=email, password=password) #adds user to DB with form inputs
         db.session.add(new_user)
         db.session.commit()
 
-        flash('Your account has been successfully created!')
+        flash('Your account has been successfully created!')                #sets session key for logged in and redirects home (now should show search!)
         session['logged_in'] = username
 
         return redirect('/')
 
+
 @app.route('/search')
 def search():
 
-    search_term = request.args.get('search')
+    search_term = request.args.get('search')                    #gets search term from search box
 
-    result = api.item_search('Books', Title=search_term, ResponseGroup='Images,ItemAttributes', paginate=False)
+    result = api.item_search('Books', Title=search_term, ResponseGroup='Images,ItemAttributes', paginate=False) #searches api with search term
 
-    result_items = result.Items.Item
+    result_items = result.Items.Item                            #'Unpacks' data from result page into a list of items
 
-    results_dict = {}
+    results_dict = collections.OrderedDict()                    #sets empty dictionary to hold result values
 
-    for index in range(len(result_items) - 1):
-        try:
+    for index in range(len(result_items)):                      #goes through every item in the list of result items by index value
+        try:                                                    #sets attributes as below for each item (title, author, etc.)
             key = index + 1
             asin = result_items[index].ASIN
             title = result_items[index].ItemAttributes.Title
@@ -111,20 +115,32 @@ def search():
             author = ''
             md_image = ''
             lg_image = ''
-        if len(title) > 0:
+        if len(title) > 0:                                      #if the result has a title, puts it into the results dictionary
             results_dict[key] = {'title': title, 'author': author, 'asin': asin, 'md_image': md_image, 'lg_image': lg_image}
 
     return render_template('search_results.html', results_dict=results_dict)
+
+@app.route('/add_book', methods=['POST'])
+def add_book():
+    """Add book to board"""
+
+    title = request.form.get('title')                           #gets book attributes (most using hidden inputs)
+    author = request.form.get('author')
+    asin = request.form.get('asin')
+    md_image = request.form.get('md_image')
+    lg_image = request.form.get('lg_image')
+
+    return "I GOTCHU. Here's your stuff. {} {} {} {} {}".format(title, author, asin, md_image, lg_image) #returns page with form info (for now)
+
 
 @app.route('/logout')
 def logout_user():
     """Logout user"""
 
-    del session['logged_in']
+    del session['logged_in']                                    #deletes logged in key from session
     flash("You are successfully logged out!")
 
     return redirect('/')
-
 
 
 if __name__ == "__main__":
